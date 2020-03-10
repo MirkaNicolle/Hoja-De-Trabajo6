76e737de690f443d49e7d4d755c01d7669c48a57
danielavillamar <46539048+danielavillamar@users.noreply.github.com>
2020-03-10 15:41:49 -0600

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,32 @@
+public class Card {
+
+    private String cardName;
+    private String cardType;
+
+    public Card (String cardName, String cardType){
+        setCardName(cardName);
+        setCardType(cardType);
+    }
+
+    public String getCardName() {
+        return cardName;
+    }
+
+    public void setCardName(String cardName) {
+        this.cardName = cardName;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String toString(){
+        return " Card Name: " + this.cardName +
+                " Card Type: " + this.cardType;
+    }
+
+}
